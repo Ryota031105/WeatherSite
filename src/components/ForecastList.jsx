a6fdf4b9edf60b084512435791788fd954b055c7
RyotaMiura1105 <a22.3ytd@g.chuo-u.ac.jp>
2026-02-23 10:02:48 +0900

    CSSの調整

--- src/components/ForecastList.jsx
+++ src/components/ForecastList.jsx
@@ -6,46 +6,48 @@ export const ForecastList = ({ forecastData }) => {
         <>
             <div className={styles.item}>
                 {forecastData && <>
-                    <div className={styles.timeAll}>
-                        <span className={styles.forecastFont}>予報</span>
-                        <span className={styles.time}>0</span>
-                        <span className={styles.time}>3</span>
-                        <span className={styles.time}>6</span>
-                        <span className={styles.time}>9</span>
-                        <span className={styles.time}>12</span>
-                        <span className={styles.time}>15</span>
-                        <span className={styles.time}>18</span>
-                        <span className={styles.time}>21</span>
-                    </div>
-                    <div className={styles.test}>
-                        <p className={styles.dayFont}>{forecastData.list[0].dt_txt.slice(8, 10)}日</p>
-                        {forecastData.list.slice(0, 8).map((data, index) => {
-                            return <ForecastItem key={index} data={data} />
-                        })}
-                    </div>
-                    <div className={styles.test}>
-                        <p className={styles.dayFont}>{forecastData.list[8].dt_txt.slice(8, 10)}日</p>
-                        {forecastData.list.slice(8, 16).map((data, index) => {
-                            return <ForecastItem key={index} data={data} />
-                        })}
-                    </div>
-                    <div className={styles.test}>
-                        <p className={styles.dayFont}>{forecastData.list[16].dt_txt.slice(8, 10)}日</p>
-                        {forecastData.list.slice(16, 24).map((data, index) => {
-                            return <ForecastItem key={index} data={data} />
-                        })}
-                    </div>
-                    <div className={styles.test}>
-                        <p className={styles.dayFont}>{forecastData.list[24].dt_txt.slice(8, 10)}日</p>
-                        {forecastData.list.slice(24, 32).map((data, index) => {
-                            return <ForecastItem key={index} data={data} />
-                        })}
-                    </div>
-                    <div className={styles.test}>
-                        <p className={styles.dayFont}>{forecastData.list[32].dt_txt.slice(8, 10)}日</p>
-                        {forecastData.list.slice(32, 40).map((data, index) => {
-                            return <ForecastItem key={index} data={data} />
-                        })}
+                    <div className={styles.innerContainer}>
+                        <div className={styles.timeAll}>
+                            <span className={styles.forecastFont}>予報</span>
+                            <span className={styles.time}>0</span>
+                            <span className={styles.time}>3</span>
+                            <span className={styles.time}>6</span>
+                            <span className={styles.time}>9</span>
+                            <span className={styles.time}>12</span>
+                            <span className={styles.time}>15</span>
+                            <span className={styles.time}>18</span>
+                            <span className={styles.time}>21</span>
+                        </div>
+                        <div className={styles.test}>
+                            <p className={styles.dayFont}>{forecastData.list[0].dt_txt.slice(8, 10)}日</p>
+                            {forecastData.list.slice(0, 8).map((data, index) => {
+                                return <ForecastItem key={index} data={data} />
+                            })}
+                        </div>
+                        <div className={styles.test}>
+                            <p className={styles.dayFont}>{forecastData.list[8].dt_txt.slice(8, 10)}日</p>
+                            {forecastData.list.slice(8, 16).map((data, index) => {
+                                return <ForecastItem key={index} data={data} />
+                            })}
+                        </div>
+                        <div className={styles.test}>
+                            <p className={styles.dayFont}>{forecastData.list[16].dt_txt.slice(8, 10)}日</p>
+                            {forecastData.list.slice(16, 24).map((data, index) => {
+                                return <ForecastItem key={index} data={data} />
+                            })}
+                        </div>
+                        <div className={styles.test}>
+                            <p className={styles.dayFont}>{forecastData.list[24].dt_txt.slice(8, 10)}日</p>
+                            {forecastData.list.slice(24, 32).map((data, index) => {
+                                return <ForecastItem key={index} data={data} />
+                            })}
+                        </div>
+                        <div className={styles.test}>
+                            <p className={styles.dayFont}>{forecastData.list[32].dt_txt.slice(8, 10)}日</p>
+                            {forecastData.list.slice(32, 40).map((data, index) => {
+                                return <ForecastItem key={index} data={data} />
+                            })}
+                        </div>
                     </div>
                 </>}
             </div>
